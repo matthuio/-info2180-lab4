79e00c38570c76b7032d0503b47b041bde290e82
Matthew <grantmatthew12345@gmail.com>
2022-11-06 20:15:12 -0500

    Excersise 2

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ window.onload= function(){
 var button = document.getElementById("btn");
 
 button.addEventListener("click",function(){
-    alert("Captain America\nIronman\nSpiderman\nCaptain Marvel\nBlack Widow\nHulk\nHawkeye\nBlack Panther\nThor\nScarlett Witch\n");
+    Req = new XMLHttpRequest();
+    var url="http://localhost/info2180-lab4/superheroes.php";
+    Req.open('GET',url);
+    Req.send();
+    Req.onreadystatechange=loadMessage; 
 });
+function loadMessage() {
+    if (Req.readyState === XMLHttpRequest.DONE) {
+      if (Req.status === 200) {
+        alert(Req.responseText);
+      } else {
+        alert('Error');
+      }
+    }
+  }
 }
--- superheroes.php
+++ superheroes.php
@@ -1,5 +1,7 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+
 $superheroes = [
   [
       "id" => 1,
