9b83a6a23e5b861b2a300135f85e681e2453bdda
Matthew <grantmatthew12345@gmail.com>
2022-11-06 19:37:03 -0500

    Excercise 2

---
+++ app.js
@@ -0,0 +1,7 @@
+window.onload= function(){
+var button = document.getElementById("btn");
+
+button.addEventListener("click",function(){
+    alert("Captain America\nIronman\nSpiderman\nCaptain Marvel\nBlack Widow\nHulk\nHawkeye\nBlack Panther\nThor\nScarlett Witch\n");
+});
+}
